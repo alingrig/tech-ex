f131f0b4b29bf55e649e29194432b9f191bdb231
Alin <alin.grig93@gmail.com>
2026-03-27 11:08:11 +0200

    Fix bblayers and use submodule

--- bootable-linux-task1/create-and-run-system.sh
+++ bootable-linux-task1/create-and-run-system.sh
@@ -13,12 +13,10 @@ LINUX_KERNEL_RECIPE=$HOME/linux-kernel-custom.bb
 
 LOCAL_CONF_SRC=$HOME/"conf/local.conf"
 QEMUX86_64_CONF_SRC=$HOME/"conf/qemux86-64.conf"
-BBLAYERS_CONF_SRC=$HOME/"conf/bblayers.conf"
 LAYER_CONF_SRC=$HOME/"conf/layer.conf"
 
 LOCAL_CONF_DST=$HOME/"bitbake-builds/poky-master/build/conf"
 QEMUX86_64_CONF_DST=$HOME/"bitbake-builds/poky-master/layers/meta-yocto/meta-custom/conf/machine"
-BBLAYERS_CONF_DST=$HOME/"bitbake-builds/poky-master/build/conf"
 LAYER_CONF_DST=$HOME/"bitbake-builds/poky-master/layers/meta-yocto/meta-custom/conf"
 
 LINUX_KERNEL_DST=$META_CUSTOM/$RECIPES_KERNEL/linux-kernel-custom/files
@@ -26,13 +24,11 @@ LINUX_KERNEL_DST=$META_CUSTOM/$RECIPES_KERNEL/linux-kernel-custom/files
 VMLINUZ="bzImage--6.12.69+git0+5b1ff7df00_a7fbaf7533-r0-genericx86-64-20260216222645.bin"
 KERNEL_MODULES="modules--6.12.69+git0+5b1ff7df00_a7fbaf7533-r0-genericx86-64-20260216222645.tgz"
 
-#yay -S --needed base-devel chrpath cpio diffstat file gawk gcc git iputils iputils-ping libacl locales python python-pip python-jinja python-pexpect python-subunit socat texinfo unzip wget xz zstd rpcsvc-proto python-websockets edk2-ovmf
+#yay -S --needed base-devel chrpath cpio diffstat file gawk gcc git iputils iputils-ping libacl locales python python-pip python-jinja python-pexpect python-subunit socat texinfo unzip wget xz zstd rpcsvc-proto python-websockets
 
-if test -d bitbake; then
-    rm -rf bitbake
+if test -d bitbake-builds; then
     rm -rf bitbake-builds
 fi
-git clone https://git.openembedded.org/bitbake
 
 ./bitbake/bin/bitbake-setup init --non-interactive poky-master poky-with-sstate distro/poky machine/qemux86-64
 
@@ -49,16 +45,16 @@ wget https://downloads.yoctoproject.org/releases/yocto/yocto-5.3.2/machines/gene
 wget https://downloads.yoctoproject.org/releases/yocto/yocto-5.3.2/machines/genericx86-64/$KERNEL_MODULES
 cd $HOME
 
-mkdir -p $QEMUX86_64_CONF_SRC
+mkdir -p $QEMUX86_64_CONF_DST
 cp $QEMUX86_64_CONF_SRC $QEMUX86_64_CONF_DST
 cp $LOCAL_CONF_SRC $LOCAL_CONF_DST
-cp $BBLAYERS_CONF_SRC $BBLAYERS_CONF_DST
 cp $LAYER_CONF_SRC $LAYER_CONF_DST
 
 source $HOME/bitbake-builds/poky-master/build/init-build-env
 
 bitbake-config-build disable-fragment machine/qemux86-64
 bitbake-config-build enable-fragment core/yocto/root-login-with-empty-password
+bitbake-layers add-layer $META_CUSTOM
 
 bitbake core-image-minimal
 
